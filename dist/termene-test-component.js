0bd5406dc1fe9ba290b452a9aa389cb837f3e471
Alex Rotaru <alexandru.rotaru@neovision.dev>
2023-07-13 17:13:21 +0300

    finalv2

--- dist/termene-test-component.js
+++ dist/termene-test-component.js
@@ -1,12 +1,12 @@
-import { resolveComponent as c, openBlock as r, createBlock as d, createElementBlock as a, Fragment as s, createVNode as p, withCtx as u, createTextVNode as m, createElementVNode as l } from "vue";
-import { createRouter as f, createWebHashHistory as h } from "vue-router";
+import { resolveComponent as c, openBlock as r, createBlock as d, createElementBlock as a, Fragment as s, createVNode as p, withCtx as l, createTextVNode as m, createElementVNode as u, toDisplayString as f } from "vue";
+import { createRouter as k, createWebHashHistory as h } from "vue-router";
 const _ = (t, o) => {
   const e = t.__vccOpts || t;
   for (const [n, i] of o)
     e[n] = i;
   return e;
 };
-const v = {
+const y = {
   __name: "App",
   setup(t) {
     return (o, e) => {
@@ -14,40 +14,40 @@ const v = {
       return r(), d(n);
     };
   }
-}, N = /* @__PURE__ */ _(v, [["__scopeId", "data-v-3ad8f193"]]), k = {}, x = /* @__PURE__ */ l("h2", null, "Home", -1);
-function y(t, o) {
+}, N = /* @__PURE__ */ _(y, [["__scopeId", "data-v-3ad8f193"]]), v = {}, b = /* @__PURE__ */ u("h2", null, "Home", -1);
+function x(t, o) {
   const e = c("router-link");
   return r(), a(s, null, [
-    x,
+    b,
     p(e, { to: { name: "about", params: { company: "awala" } } }, {
-      default: u(() => [
+      default: l(() => [
         m("about")
       ]),
       _: 1
     })
   ], 64);
 }
-const b = /* @__PURE__ */ _(k, [["render", y]]), w = {}, A = /* @__PURE__ */ l("h2", null, "About", -1);
-function g(t, o) {
-  const e = c("router-view");
+const g = /* @__PURE__ */ _(v, [["render", x]]), A = {};
+function H(t, o) {
+  const e = c("router-link");
   return r(), a(s, null, [
-    A,
+    u("h2", null, "About:" + f(t.$route.params.company), 1),
     p(e, { to: "/marketplace/companies" }, {
-      default: u(() => [
+      default: l(() => [
         m("go back")
       ]),
       _: 1
     })
   ], 64);
 }
-const H = /* @__PURE__ */ _(w, [["render", g]]), $ = [
-  { path: "/marketplace/companies", component: b },
+const w = /* @__PURE__ */ _(A, [["render", H]]), $ = [
+  { path: "/marketplace/companies", component: g },
   {
     path: "/marketplace/companies/:company",
     name: "about",
-    component: H
+    component: w
   }
-], V = f({
+], V = k({
   history: h(),
   routes: $
 });
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,4 +1,4 @@
 <template>
-  <h2>About</h2>
-  <router-view to="/marketplace/companies">go back</router-view>
+  <h2>About:{{ $route.params.company }}</h2>
+  <router-link to="/marketplace/companies">go back</router-link>
 </template>
